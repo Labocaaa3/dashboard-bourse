f23d9942703ec3c5c9bd7a877bcca1bcb9c3e6d1
Labocaaa3 <louis.bouchet2003@gmail.com>
2025-04-11 15:48:06 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -55,4 +55,4 @@ def update_graph(_):
 
 # Lancement du serveur Dash sur 0.0.0.0:8050
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8050)
