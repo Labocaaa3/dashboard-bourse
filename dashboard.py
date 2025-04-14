52343bc3cd00e61834476abb802e28044b2c2b87
Labocaaa3 <louis.bouchet2003@gmail.com>
2025-04-14 14:32:16 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,9 +11,9 @@ app = Dash(__name__)
 def load_data():
     if os.path.exists('eurostoxx50_data.csv'):
         df = pd.read_csv('eurostoxx50_data.csv', header=0)
-        df.columns = df.columns.str.strip()  # Nettoyer les espaces
+        df.columns = df.columns.str.strip() 
 
-        print("[DEBUG] Colonnes CSV:", df.columns.tolist())  # Optionnel pour vérifier
+        print("[DEBUG] Colonnes CSV:", df.columns.tolist()) 
 
         df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
         df = df.sort_values(by='Date')
